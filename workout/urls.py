65748871c3e516f557d0bd442921ad5474da579b
reda97dz <rbaka79@gmail.com>
2021-09-19 23:13:43 +0100

    update urls

--- workout/urls.py
+++ workout/urls.py
@@ -14,6 +14,6 @@ urlpatterns = [
     path('add_new_run/', views.add_run, name='add_run'),
     path('run/<str:pk>/delete/', views.delete_run, name='delete_run'),
     path('run/<str:pk>/edit/', views.edit_run, name='edit_run'),
-    path('date/<int:year>/', views.run_list, name='run_list_by_year'),
-    path('date/<int:year>/<int:month>', views.run_list, name='run_list_by_year_month'),
+    path('runs/date/<int:year>/', views.run_list, name='run_list_by_year'),
+    path('runs/date/<int:year>/<int:month>', views.run_list, name='run_list_by_year_month'),
 ]
